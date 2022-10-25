89e07398cd59aced7691575d26096a0936c2c818
HangHang <dhkdwlsgod@naver.com>
2022-10-25 23:47:25 +0900

    ✨ feat(stock): #S07P31B204-121 스프링 환경설정 세팅
    스프링 스타터로 생성, 몽고db, mysql 매핑이 안되있어서 실행이 되진 않음

---
+++ backend/stock/src/test/java/motoo/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package motoo.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
