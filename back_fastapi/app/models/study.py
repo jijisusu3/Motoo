e0419d3561c5dba1c195f2f36de025dcf4042ecc
장진세 <holy_killer@naver.com>
2022-10-27 15:16:20 +0900

    fix(models): fix wrong field names

--- back_fastapi/app/models/study.py
+++ back_fastapi/app/models/study.py
@@ -1,12 +1,13 @@
 from asyncio.windows_events import NULL
 from tortoise import fields, Model
 
+
 class Quiz(Model):
-  question = fields.CharField(max_length=200)
-  examples = fields.CharField(max_length=200)
-  answer = fields.CharField(max_length=20)
+    question = fields.CharField(max_length=200)
+    examples = fields.CharField(max_length=200)
+    answer = fields.CharField(max_length=20)
 
 
 class Voca(Model):
-  voca = fields.CharField(max_length=100)
-  explanation = fields.CharField(max_length=255)
+    voca = fields.CharField(max_length=100)
+    explanation = fields.CharField(max_length=255)
