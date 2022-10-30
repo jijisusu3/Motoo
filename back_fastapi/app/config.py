f229310bf99c115d3a79194974d6b80373ac26f3
장진세 <holy_killer@naver.com>
2022-10-30 22:42:15 +0900

    fix[config.py]: change env vars

--- back_fastapi/app/config.py
+++ back_fastapi/app/config.py
@@ -1,22 +1,32 @@
+import os
+
 from pydantic import BaseSettings
 import redis
 
 
 class Settings(BaseSettings):
-    DB_URL: str = ""
-    ROOT_PASSWORD: str = ""
-    OPEN_API_DOMAIN: str = ""
-    CANDLE_API_URL: str = ""
-    APPKEY_FOR_CANDLE: str = ""
-    APPSECRET_FOR_CANDLE: str = ""
-    AUTHORIZATION_FOR_CANDLE: str = ""
-    TRADE_ID_FOR_CANDLE: str = ""
-    REDIS_HOST: str = ""
-    REDIS_PORT: int
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    DB_URL = os.environ.get("DB_URL")
+    ROOT_PASSWORD = os.environ.get("ROOT_PASSWORD")
+    OPEN_API_DOMAIN = os.environ.get("OPEN_API_DOMAIN")
+    CANDLE_API_URL = os.environ.get("CANDLE_API_URL")
+    APPKEY_FOR_CANDLE = os.environ.get("APPKEY_FOR_CANDLE")
+    APPSECRET_FOR_CANDLE = os.environ.get("APPSECRET_FOR_CANDLE")
+    TRADE_ID_FOR_CANDLE = os.environ.get("TRADE_ID_FOR_CANDLE")
+    REDIS_HOST = os.environ.get("REDIS_HOST")
+    REDIS_PORT = os.environ.get("REDIS_PORT")
+    # DB_URL: str = ""
+    # ROOT_PASSWORD: str = ""
+    # OPEN_API_DOMAIN: str = ""
+    # CANDLE_API_URL: str = ""
+    # APPKEY_FOR_CANDLE: str = ""
+    # APPSECRET_FOR_CANDLE: str = ""
+    # TRADE_ID_FOR_CANDLE: str = ""
+    # REDIS_HOST: str = ""
+    # REDIS_PORT: int
+    #
+    # class Config:
+    #     env_file = ".env"
+    #     env_file_encoding = "utf-8"
 
 
 settings = Settings()
