7cf55f2a61c6d94ca35cfc55e20d64ea6aa8d461
장진세 <holy_killer@naver.com>
2022-10-31 01:31:06 +0900

    feat[stocks.py]: create pydantic schemas for stock detail

--- back_fastapi/app/schemes/stocks.py
+++ back_fastapi/app/schemes/stocks.py
@@ -1,13 +1,54 @@
 import datetime
+from typing import List
 from pydantic import BaseModel, Field, validator
 
 
-class CandleData(BaseModel):
+class ChartBase(BaseModel):
+    id: int
     stock_id: int
     date: str
-    time: str
-    price: int
-    volume: int
-    open_price:int
     max_price: int
     min_price: int
+    volume: int
+    open_price: int
+
+
+class CandleData(ChartBase):
+    time: str
+    price: int
+
+
+class DayChartData(ChartBase):
+    close_price: int
+
+
+class ShortStockData(BaseModel):
+    id: int
+    price: int
+    fluctuation_rate: float
+    fluctuation_price: int
+    volume: int
+    trading_value: int
+    daily: List[CandleData]
+
+
+class EntireStockData(ShortStockData):
+    ticker: str
+    category_id: int
+    category_name: str
+    close_price: int
+    maximum: int
+    minimum: int
+    per: float
+    roe: float
+    eps: float
+    m_capital: int
+    issued: int
+    capital: int
+    weekly: List[CandleData]
+    monthly: List[DayChartData]
+    yearly: List[DayChartData]
+
+
+
+
