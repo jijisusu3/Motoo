29df7d49ba0609a7cc67693c71806793e949e2d4
장진세 <holy_killer@naver.com>
2022-11-02 13:35:04 +0900

    feat[commands]: # S07P31B204-286 add save-token command

--- back_fastapi/app/commands/daily.py
+++ back_fastapi/app/commands/daily.py
@@ -1,8 +1,10 @@
 import time
+import requests
 from datetime import date, timedelta
 from collections import defaultdict
 from app.models.stocks import Stock, Category
 from app.const import *
+from app.config import settings, redis_session
 from pykrx import stock
 import asyncio
 import typer
@@ -42,6 +44,31 @@ async def insert_daily():
         await day_map[ctgr].bulk_create(ctgr_dict[ctgr])
     end = time.time()
     print(end-start)
+
+
+def get_auth_token(key, secret):
+    url = settings.OPEN_API_DOMAIN+'/oauth2/tokenP'
+    request_body = {
+        "grant_type": "client_credentials",
+        "appkey": key,
+        "appsecret": secret
+    }
+    res = requests.post(url, json=request_body)
+    res_code = res.status_code
+    if res_code == 200:
+        return res.json()["access_token"]
+    else:
+        print("Error Code : " + str(res_code) + " | " + res.text)
+        return None
+
+
+@app.command()
+def save_token(token_use: str = 'update_stock'):
+    now = time.time()
+    res = get_auth_token(settings.APPKEY_FOR_CANDLE, settings.APPSECRET_FOR_CANDLE)
+    if res is not None:
+        redis_session.rpush(token_use, res, now)
+        redis_session.expire(token_use, 43200)
 
 
 if __name__ == "__main__":
