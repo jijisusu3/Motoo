08bf33b5dd5d76b4d1d39daf776c0a3de3257701
wltn7498@naver,com <wltn7498@naver.com>
2022-11-04 09:37:13 +0900

    feat: 리덕스 상태관리 설정/사용, 소셜로그인 기능 완성, 로그인/로그아웃/Page Block기능 완성

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
+import store from './store'
 import "./styles/index.css";
 import App from "./App";
-// import reportWebVitals from './reportWebVitals';
-// import store, { persistor } from './store/index';
-// import { PersistGate } from 'redux-persist/integration/react';
 
 import "./styles/index.css";
 import "./styles/minireset.min.css";
@@ -25,43 +23,16 @@ export default function ScrollToTop() {
   return null;
 }
 
-// export default function NavShow() {
-//   const [showNav, setShowNav] = useState(true)
-//   const { pathname } = useLocation();
-//   function checkShowNav(u) {
-//     const { nowPath } = JSON.stringify(u)
-//     const tmpList = [
-//       "/stock/limit-order/buy",
-//       "/stock/limit-order/sell",
-//       "/stock/buy",
-//       "/stock/sell",
-//       "/stock/industry",
-//     ];
-//     if (nowPath === '/stock/buy') {
-//       setShowNav(false)
-//     } else {
-//       setShowNav(true)
-//       return
-//     }
-//   }
-//   console.log(pathname)
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//     checkShowNav(pathname)
-//   }, [pathname])
-//   return console.log('ㅠㅠㅠㅠ')
-// }
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <ScrollToTop />
       <BottomNav />
       <App />
       <FooterBar />
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- frontend/src/pages/MenuPage.jsx
+++ frontend/src/pages/MenuPage.jsx
@@ -1,7 +1,14 @@
+import { useDispatch } from "react-redux";
+import { setLogout } from "../stores/userSlice";
+
 function MenuPage() {
+  const dispatch = useDispatch()
+  function logoutClick() {
+    dispatch(setLogout())
+  }
   return (
     <div>
-      <h1>메뉴페이지임니다</h1>
+      <button onClick={logoutClick}>로그아웃 되나 볼거야</button>
     </div>
   );
 }
