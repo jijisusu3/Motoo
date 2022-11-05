2145586e9fa6bec67a2ce1595dde186ed9f35667
HangHang <dhkdwlsgod@naver.com>
2022-11-05 16:12:22 +0900

    ✨ feat(trading service): 트레이딩 테이블 기본 세팅

---
+++ backend/stock/src/main/java/com/motoo/api/service/TradingServiceImpl.java
@@ -0,0 +1,31 @@
+package com.motoo.api.service;
+
+import com.motoo.db.entity.Trading;
+
+import java.util.List;
+
+public class TradingServiceImpl implements TradingService{
+
+
+
+
+    @Override
+    public void writeOrder(Long userId, Long accountId, int price, int amount) {
+
+    }
+
+    @Override
+    public List<Trading> tradingList() {
+        return null;
+    }
+
+    @Override
+    public void updateOrder() {
+
+    }
+
+    @Override
+    public void deleteOrder() {
+
+    }
+}
