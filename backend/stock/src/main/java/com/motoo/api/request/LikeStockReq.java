12a8f27abc4a94d808999e81d7df81aa7477bad9
이수랑 <rang628@naver.com>
2022-11-05 19:02:31 +0900

    🐛 Bug: aPI 파라미터 받을때 값을 JSON 형식으로 받도록 수정

--- backend/stock/src/main/java/com/motoo/api/request/LikeStockReq.java
+++ backend/stock/src/main/java/com/motoo/api/request/LikeStockReq.java
@@ -3,7 +3,10 @@ package com.motoo.api.request;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-public class UpdateUserPutRequest {
+@Getter
+@Setter
+public class LikeStockReq {
+
+    Long stockId;
 
 }
