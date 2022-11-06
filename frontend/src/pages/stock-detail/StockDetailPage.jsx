a7767464babd500b73959bd1f86f46f856b28285
wltn7498@naver,com <wltn7498@naver.com>
2022-11-06 22:48:22 +0900

    feat(StockDetailPage.jsx): websocket 경로 수정

--- frontend/src/pages/stock-detail/StockDetailPage.jsx
+++ frontend/src/pages/stock-detail/StockDetailPage.jsx
@@ -21,7 +21,7 @@ function StockDetailPage() {
   });
 
   useEffect(() => {
-    const wss = new WebSocket("wss://k7b204.p.ssafy.io:8080/api1/socket/wss");
+    const wss = new WebSocket("wss://k7b204.p.ssafy.io:8080/api1/socket/ws");
     wss.onopen = () => {
       wss.send("전지수 보이삼보이삼?");
     };
