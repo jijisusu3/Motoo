7ee38f7e4e488dd34c7954db30e82db9b1a6ee25
zyyyyysoo <newsty1107@gmail.com>
2022-11-07 00:32:56 +0900

    feat: stocklist router 수정

--- back_fastapi/app/routers/list.py
+++ back_fastapi/app/routers/list.py
@@ -3,6 +3,15 @@
 # from app.models.stocks import Stock
 
 # from fastapi import APIRouter, Response
-# from app.schemes.categories import GetCategoryDetailResponse
+# from app.schemes.lists import GetFavoriteStockList, GetTopStockList
 
 # router = APIRouter(prefix="/list")
+
+# @router.get("/list/{user_id}", description="관심 종목 리스트", response_model=GetFavoriteStockList)
+# async def get_favorite_list(user_id: int, response: Response):
+#     try:
+#         user = await User.get(id=user_id)
+#     except tortoise.exceptions.DoesNotExist:
+#         response.status_code = 404
+#         return GetFavoriteStockListResponse(message="failed")
+#     stocks = await stock[]
--- back_fastapi/app/schemes/lists.py
+++ back_fastapi/app/schemes/lists.py
@@ -1,22 +1,22 @@
-# from typing import Union
-# from pydantic import BaseModel, Field
-# from app.schemes.common import CommonResponse
+from typing import Union, List
+from pydantic import BaseModel, Field
+from app.schemes.common import CommonResponse
 
-# class StockList(BaseModel):
-#   id: int
-#   fluctuation_rate: Union[float, None] = Field(description="등락률")
-#   price: Union[int, None] = Field(description="현재가")
-#   name: Union[str, None] = Field(description="종목이름")
-#   ticker: Union[str, None]
-
-
-# class Favorite(StockList):
-#   user_id: int
-#   stock
+class StockList(BaseModel):
+  id: int
+  fluctuation_rate: Union[float, None] = Field(description="등락률")
+  price: Union[int, None] = Field(description="현재가")
+  name: Union[str, None] = Field(description="종목이름")
+  ticker: Union[str, None]
 
 
-# class GetFavoriteStockList(Favorite, CommonResponse):
-#   pass
+class Favorite(BaseModel):
+  user_id: int
+  stocks: List[StockList]
 
-# class GetTopStockList(StockList, CommonResponse):
-#   pass
+
+class GetFavoriteStockList(Favorite, CommonResponse):
+  pass
+
+class GetTopStockList(StockList, CommonResponse):
+  pass
