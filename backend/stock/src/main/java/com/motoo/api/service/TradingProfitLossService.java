7a6b0da5b7627c8d6c3f7fde1e9ee9646a62d639
이수랑 <rang628@naver.com>
2022-11-10 00:56:36 +0900

    ✨ Feat(accountdetailcontroller.java etc.): 자산 포트폴리오, 계좌자산, 판매손익, 거래내역을 포함한 계좌 상세 정보 조회 구현

--- backend/stock/src/main/java/com/motoo/api/service/TradingProfitLossService.java
+++ backend/stock/src/main/java/com/motoo/api/service/TradingProfitLossService.java
@@ -2,6 +2,7 @@ package com.motoo.api.service;
 
 import com.motoo.api.dto.accountDetail.TradingProfitLoss;
 import com.motoo.api.dto.accountDetail.TradingStock;
+import com.motoo.db.entity.Trading;
 
 import java.util.List;
 
@@ -9,8 +10,10 @@ public interface TradingProfitLossService {
     /**
      * 판매손익
      */
-    TradingProfitLoss getTradingProfitLoss();
-    List<TradingStock> getStockOrderByTradingPL();
-    List<TradingStock> getStockStockOrderByTradingPLRatio();
-    TradingStock getTradingStock();
+    TradingProfitLoss getTradingProfitLoss(Long userId, Long accountId);
+    List<TradingStock> getStockOrderByTradingPL(Long userId, Long accountId);
+    List<TradingStock> getStockOrderByTradingPLRatio(Long userId, Long accountId);
+    List<TradingStock> getTradingStockList(Long userId, Long accountId);
+
+    List<Trading> test(Long userId, Long accountId);
 }
