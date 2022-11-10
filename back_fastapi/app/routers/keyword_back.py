b59b5dc2c9ef3d4ca63def868e3cd6bff05dd850
Jisoo Jeon <newsty1107@gmail.com>
2022-11-10 15:14:58 +0900

    🐛 Bug(keyword_back.py): #S07P31B204-387 키워드 함수 수정
    failed case 일때 null 값 반환

--- back_fastapi/app/routers/keyword_back.py
+++ back_fastapi/app/routers/keyword_back.py
@@ -18,8 +18,6 @@ router = APIRouter(prefix="/keyword_back")
 tokenizer = AutoTokenizer.from_pretrained("snunlp/KR-FinBert-SC")
 model = AutoModelForSequenceClassification.from_pretrained("snunlp/KR-FinBert-SC")
 
-file = open('app/ignore.txt', 'r', encoding='UTF8')
-delete_word = file.read().splitlines()
 
 file = open('app/company_list.txt', 'r', encoding='UTF8')
 company_lst = file.read().splitlines()
@@ -61,6 +59,8 @@ def clean(x):
 @router.get("/category")
 async def get_category_keyword():
     tm = time.time()
+    file = open('app/ignore_category.txt', 'r', encoding='UTF8')
+    delete_word = file.read().splitlines()
     for i in range(len(category_lst)):
         category = await Category.get(id=i+1)
         url = settings.NAVER_API_DOMAIN + str(category_lst[i]) + '&display=100&start=1&sort=sim'
@@ -150,6 +150,8 @@ async def get_category_sentiment():
 @router.get("/company")
 async def get_company_keyword():
     tm = time.time()
+    file = open('app/ignore_company.txt', 'r', encoding='UTF8')
+    delete_word = file.read().splitlines()
     for i in range(len(company_lst)):
         keyword = await Keyword.get(id=i+1)
         url = settings.NAVER_API_DOMAIN + str(company_lst[i]) + '&display=100&start=1&sort=sim'
