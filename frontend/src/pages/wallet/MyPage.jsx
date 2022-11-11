dc7a59e0b67b76ea13ee5bfd934540d1a787ade2
Jisoo Jeon <newsty1107@gmail.com>
2022-11-12 00:15:52 +0900

    ✨ Feat(mypage.jsx): css 수정, 모달 만들기
    계좌 변경 모달

--- frontend/src/pages/wallet/MyPage.jsx
+++ frontend/src/pages/wallet/MyPage.jsx
@@ -16,11 +16,11 @@ const style = {
   top: "40%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 344,
-  height: 360,
+  width: 324,
+  height: 225,
   bgcolor: "background.paper",
   boxShadow: 24,
-  p: 1,
+  p: 4,
   borderRadius: 5,
 };
 
@@ -439,10 +439,24 @@ function MyPage() {
           </Box>
         </Modal>
         <Modal open={openChangeModal} onClose={handleChangeModalClose}>
-          <Box sx={style}>
-            <div>정말 변경하시겠습니까?</div>
-            <div>계좌를 변경하면 ~~~되어요!</div>
-            <button onClick={changeSubmit}>변경하기</button>
+          <Box className={classes.deletebox} sx={style}>
+            <div className={classes.title}>계좌를 변경하시겠습니까?</div>
+            <div className={classes.graybox}>
+            <svg
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M7.43866e-05 9.99846C7.43866e-05 4.47648 4.4766 0 9.99857 0C15.5206 0 19.9971 4.47648 19.9971 9.99846C19.9971 11.641 19.5999 13.2274 18.8523 14.6481L19.9685 18.9358C20.0051 19.0762 20.0051 19.2237 19.9685 19.3642C19.8502 19.8184 19.3861 20.0906 18.9319 19.9724L14.6421 18.8554C13.2229 19.601 11.6387 19.997 9.99857 19.997C4.4766 19.997 7.43866e-05 15.5205 7.43866e-05 9.99846ZM9.99857 4.50078C9.58443 4.50078 9.24868 4.83652 9.24868 5.25067V11.4997C9.24868 11.9139 9.58443 12.2496 9.99857 12.2496C10.4127 12.2496 10.7485 11.9139 10.7485 11.4997V5.25067C10.7485 4.83652 10.4127 4.50078 9.99857 4.50078ZM8.99872 14.4972C8.99872 15.0493 9.44635 15.497 9.99857 15.497C10.5508 15.497 10.9984 15.0493 10.9984 14.4972C10.9984 13.945 10.5508 13.4973 9.99857 13.4973C9.44635 13.4973 8.99872 13.945 8.99872 14.4972Z"
+                  fill="#8D8D8D"
+                />
+              </svg>
+              <div style={{ marginLeft: "15px" }}>계좌를 변경하면<br />해당 계좌에서 모든 거래가 이루어져요.</div>
+            </div>
+            <button className={classes.btn} onClick={changeSubmit}>변경하기</button>
           </Box>
         </Modal>
         <div className={classes.addbtn}>
