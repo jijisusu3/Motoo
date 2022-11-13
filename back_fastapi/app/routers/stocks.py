0e0011342fcf33454e4453f5296fe2be0325c3ab
Hamelin92 <holy_killer@naver.com>
2022-11-13 17:05:01 +0900

    🚑 Quickfix(routers/stocks.py): redis 데이터 형식에 맞지 않아 발생하던 오류 수정

--- back_fastapi/app/routers/stocks.py
+++ back_fastapi/app/routers/stocks.py
@@ -93,8 +93,8 @@ async def get_trading_stock_info(ticker: str, response: Response):
 
 @router.get("/bidask/{ticker}", description="매수매도호가 조회", response_model=BidAskResponse)
 async def get_bidask_list(ticker: str):
-    bid_list = redis_session.lrange('bid' + ticker, 0, 9)
-    ask_list = redis_session.lrange('ask' + ticker, 0, 9)
+    bid_list = redis_session.lrange('bid_' + ticker, 0, 9)
+    ask_list = redis_session.lrange('ask_' + ticker, 0, 9)
     access_token = redis_session.get("get_bidask")
     if access_token is None:
         save_token("get_bidask")
@@ -109,18 +109,21 @@ async def get_bidask_list(ticker: str):
         )
         if res.status_code == 200:
             data = res.json()['output1']
-            for i in range(1, 11):
-                redis_session.rpush('ask' + ticker, {"price": data[f'askp{i}'], "rsqn": data[f'askp_rsqn{i}']})
-                redis_session.rpush('bid' + ticker, {"price": data[f'bidp{i}'], "rsqn": data[f'bidp_rsqn{i}']})
-                redis_session.expire('ask' + ticker, 60)
-                redis_session.expire('bid' + ticker, 60)
-            bid_list = sorted(redis_session.lrange('bid' + ticker, 0, 9), key=lambda x: x["price"], reverse=True)[:5]
-            ask_list = sorted(redis_session.lrange('ask' + ticker, 0, 9), key=lambda x: x["price"])[:5]
+            for i in range(1, 6):
+                redis_session.rpush('ask_' + ticker, data[f'askp{i}'])
+                redis_session.rpush('bid_' + ticker, data[f'bidp{i}'])
+            for i in range(1,6):
+                redis_session.rpush('ask_' + ticker, data[f'askp_rsqn{i}'])
+                redis_session.rpush('bid_' + ticker, data[f'bidp_rsqn{i}'])
+            redis_session.expire('ask_' + ticker, 60)
+            redis_session.expire('bid_' + ticker, 60)
+            bid_list = redis_session.lrange('bid_' + ticker, 0, 9)
+            ask_list = redis_session.lrange('ask_' + ticker, 0, 9)
     return BidAskResponse(
-        bid_pr=[bid["price"] for bid in bid_list],
-        ask_pr=[ask["price"] for ask in ask_list],
-        bid_rsqn=[bid["rsqn"] for bid in bid_list],
-        ask_rsqn=[ask["rsqn"] for ask in ask_list]
+        bid_pr=bid_list[:5],
+        ask_pr=ask_list[:5],
+        bid_rsqn=bid_list[5:],
+        ask_rsqn=ask_list[5:]
     )
 
 
