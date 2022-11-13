cbb37e7316355bd109ab5da246eb78227b405409
ys <ysltub@gmail.com>
2022-11-13 21:54:27 +0900

    🐛 Bug(캐로셀 임포트 fix): 캐로셀 임포트 문제 fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,8 +9,7 @@ import App from "./App";
 import "./styles/index.css";
 import "./styles/minireset.min.css";
 import "./styles/pretendard.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
