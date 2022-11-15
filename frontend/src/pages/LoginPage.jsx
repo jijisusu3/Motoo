28384310adf9a8c87120a118ede337ac1fc01ada
wltn7498@naver,com <wltn7498@naver.com>
2022-11-15 14:12:58 +0900

    feat(LoginPage.jsx): login 경로 재설정

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -24,7 +24,7 @@ function LoginPage() {
     dispatch(setShowNav(now))
   })
   const REST_API_KEY = "0bb59360a92e431731e74ae18fe21a68";
-  const REDIRECT_URI = "http://localhost:3000//social-login";
+  const REDIRECT_URI = "http://localhost:3000/social-login";
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   function kakaoLoginClick() {
     window.location.replace(KAKAO_AUTH_URI)
