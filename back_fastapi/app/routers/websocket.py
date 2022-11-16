d6b37da89c122544c7846ef76251b76cec79f882
Jisoo Jeon <newsty1107@gmail.com>
2022-11-16 14:22:24 +0900

    ✨ Feat(websocket.py): 웹소켓 데이터 send 시간 설정
    홀수분마다 한번씩 send 하도록 수정

--- back_fastapi/app/routers/websocket.py
+++ back_fastapi/app/routers/websocket.py
@@ -1,5 +1,7 @@
 import tortoise
 from fastapi import APIRouter, Response, WebSocket
+import time
+from datetime import datetime
 
 router = APIRouter(prefix="/socket")
 
@@ -8,8 +10,13 @@ router = APIRouter(prefix="/socket")
 async def websocket_endpoint(websoket: WebSocket):
     await websoket.accept()
     while True:
-        data = await websoket.receive_text()
-        print(data)
-        await websoket.send_json({
-            "result": True
-        })
+        week = datetime.now().weekday()
+        hour = datetime.now().hour
+        min = datetime.now().minute
+        sec = datetime.now().second
+        if week < 5:
+            if 9 <= hour <=16 and min % 2 == 1 and sec == 1:
+                await websoket.send_json({
+                    "result": True
+                })
+        time.sleep(1)
