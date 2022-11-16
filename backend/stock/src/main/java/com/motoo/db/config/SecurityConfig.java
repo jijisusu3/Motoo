afac552a82f70bb80e985c9f183e012001e6cb12
이수랑 <rang628@naver.com>
2022-11-16 11:05:28 +0900

    ✨ Feat(securityconfig.java): 스프링시큐리티 인증 필요한 페이지 설정

--- backend/stock/src/main/java/com/motoo/db/config/SecurityConfig.java
+++ backend/stock/src/main/java/com/motoo/db/config/SecurityConfig.java
@@ -39,9 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()
-                .antMatchers("/api/v1/users/test/*").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
-                .antMatchers("/api/admin/**").hasRole("ADMIN")
-                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("api2/users/auth/**").permitAll()
+                .antMatchers("/api2/**").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
+//                .antMatchers("/api/admin/**").hasRole("ADMIN")
+//                .antMatchers("/swagger-ui/**").permitAll()
                 .anyRequest().permitAll();
 //                .and().cors();
     }
