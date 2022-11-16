ac66b73daa537009a334f063a0bc96bed828b13a
이수랑 <rang628@naver.com>
2022-11-16 11:29:45 +0900

    🐛 Bug(securityconfig.java): 인가 필요없는 api url 수정

--- backend/stock/src/main/java/com/motoo/db/config/SecurityConfig.java
+++ backend/stock/src/main/java/com/motoo/db/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), userService)) //HTTP 요청에 JWT 토큰 인증 필터를 거치도록 필터를 추가
                 .authorizeRequests()
-                .antMatchers("api2/users/auth/**").permitAll()
+                .antMatchers("/api2/users/auth/**").permitAll()
                 .antMatchers("/api2/**").authenticated()       //인증이 필요한 URL과 필요하지 않은 URL에 대하여 설정
 //                .antMatchers("/api/admin/**").hasRole("ADMIN")
 //                .antMatchers("/swagger-ui/**").permitAll()
