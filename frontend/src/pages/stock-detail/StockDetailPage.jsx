28c4eacb4cbd51d4112eca299623cbf7e9824c91
ys <ysltub@gmail.com>
2022-11-17 11:17:31 +0900

    💄 UI(팔래요 페이지 및 메뉴 페이지 및 디테일 버튼 수정): cSS 수정

--- frontend/src/pages/stock-detail/StockDetailPage.jsx
+++ frontend/src/pages/stock-detail/StockDetailPage.jsx
@@ -10,6 +10,7 @@ import { setShowNav } from "../../stores/navSlice";
 import { stockDetailGet } from "../../stores/stockSlice";
 import { likeStockPost, realtimeAccountGet } from "../../stores/userSlice";
 import { width } from "@mui/system";
+import { ButtonBase } from "@mui/material";
 
 function StockDetailPage() {
   const params = useParams();
@@ -1105,29 +1106,25 @@ function StockDetailPage() {
   function BuySellButton() {
     if (showSellButton) {
       return (
-        <div style={{ width: "100%" }}>
-          <div className={classes.sellbuy}>
-            <div className={classes.flx}>
-              <Link to={`/stock/sell/${id}`}>
-                <button style={{ marginRight: "5px" }} className={classes.sell}>
-                  팔래요
-                </button>
-              </Link>
-            </div>
-            <div className={classes.flx}>
-              <Link to={`/stock/buy/${id}`}>
-                <button className={classes.buy}>살래요</button>
-              </Link>
-            </div>
+        <div className={classes.buttons}>
+          <div className={classes.buysell}>
+            <Link to={`/stock/sell/${id}`}>
+              <button style={{backgroundColor:"#7BCDC8", marginRight: "8px"}} className={classes.buysellbutton}>팔래요</button>
+            </Link>
+            <Link to={`/stock/buy/${id}`}>
+              <button style={{backgroundColor:"#FECE6D"}} className={classes.buysellbutton}>살래요</button>
+            </Link>
           </div>
         </div>
       );
     }
     return (
-      <div className={classes.onlysellbuy}>
-        <Link to={`/stock/buy/${id}`} state={{ data: shortStockData }}>
-          <button className={classes.onlybuy}>살래요</button>
-        </Link>
+      <div className={classes.buttons}>
+        <div className={classes.buysell}>
+          <Link to={`/stock/buy/${id}`} state={{ data: shortStockData }}>
+            <button className={classes.sellbutton}>살래요</button>
+          </Link>
+        </div>
       </div>
     );
   }
