ba5d589a02d044abdcc99a680bd173f54760af8b
ys <ysltub@gmail.com>
2022-11-18 21:06:57 +0900

    🚑 Quickfix(퀴즈 중복 방지 및 시간 설정): 퀴즈 중복 방지 및 시간 설정

--- backend/stock/src/main/java/com/motoo/api/controller/QuizController.java
+++ backend/stock/src/main/java/com/motoo/api/controller/QuizController.java
@@ -4,9 +4,12 @@ import com.motoo.api.request.QuizReq;
 import com.motoo.api.service.QuizService;
 import com.motoo.api.service.UserService;
 import com.motoo.common.model.response.BaseResponseBody;
+import com.motoo.db.entity.User;
+import com.motoo.db.repository.UserRepository;
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.datasource.IsolationLevelDataSourceAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -21,6 +25,7 @@ import java.util.Date;
 @RestController
 @RequiredArgsConstructor
 public class QuizController {
+    private final UserRepository userRepository;
 
     private final UserService userService;
     private final QuizService quizService;
@@ -28,12 +33,25 @@ public class QuizController {
     public ResponseEntity<?> ReadQuiz() { return ResponseEntity.ok(quizService.getQuiz()); }
     @PutMapping("/api2/quiz")
     public ResponseEntity SolveQuiz(Authentication authentication, @RequestBody QuizReq quizReq) {
-        Long id = userService.getUserIdByToken(authentication);
-        Date now = java.sql.Date.valueOf(LocalDate.now());
-        userService.updateQuizDay(id, now);
-        String result = quizService.solveQuiz(id, quizReq);
 
-        return ResponseEntity.status(200).body(BaseResponseBody.of(200, result));
+        Long id = userService.getUserIdByToken(authentication);
+        User user = userRepository.findByUserId(id).get();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date quiz = user.getQuizDay();
+        String quizdate = format.format(quiz);
+        Date now = java.sql.Date.valueOf(LocalDate.now());
+        String nowdate = format.format(now);
+        System.out.println(quizdate);
+        System.out.println(nowdate);
+        if (quizdate.equals(nowdate)) {
+            return ResponseEntity.status(400).body(BaseResponseBody.of(400, "욕심쟁이"));
+        } else {
+            userService.updateQuizDay(id, now);
+            String result = quizService.solveQuiz(id, quizReq);
+            return ResponseEntity.status(200).body(BaseResponseBody.of(200, result));
+        }
+
+
     }
 
 }
