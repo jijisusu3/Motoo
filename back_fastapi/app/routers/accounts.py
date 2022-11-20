7b602ff5ac6953e283e0a38fbf05a5c8d4a7b55a
장진세 <holy_killer@naver.com>
2022-11-21 03:30:47 +0900

    ✨ Feat(accounts.py): 몇가지 예외적인 상황에서 정상적으로 데이터 보내도록 처리

--- back_fastapi/app/routers/accounts.py
+++ back_fastapi/app/routers/accounts.py
@@ -16,15 +16,15 @@ router = APIRouter(prefix="/account")
 @router.get("/check/{account_id}",
             description="해당 계좌의 보유한 주식, 보유량 조회",
             response_model=CheckAccountStockResponse)
-async def check_account_stock(account_id: int, response: Response = 200, user: User = Depends(get_current_user)):
+async def check_account_stock(account_id: int, user: User = Depends(get_current_user), response: Response = 200):
     response.status_code = 200
     try:
         account = await Account.get(id=account_id, user_id=user.pk)
+        seed = account.seed
         account_stocks = await account.account_stock
         selling = await Trading.filter(account_id=account_id, user_id=user.pk, tr_type=3)
         buying = await Trading.filter(account_id=account_id, user_id=user.pk, tr_type=4)
         acc_stck_map = dict()
-        seed = account.seed
         for ac_st in account_stocks:
             ac_ticker = (await Stock.get(id=ac_st.stock_id)).ticker
             acc_stck_map[ac_ticker] = AccountStockInfo(stockId=ac_st.stock_id,
@@ -32,7 +32,8 @@ async def check_account_stock(account_id: int, response: Response = 200, user: U
                                                        amount=ac_st.amount,
                                                        available=ac_st.amount)
         for sell in selling:
-            acc_stck_map[sell.ticker].available -= sell.tr_amount
+            if acc_stck_map.get(sell.ticker) is not None:
+                acc_stck_map.get(sell.ticker).available -= sell.tr_amount
         for buy in buying:
             seed -= buy.tr_price * buy.tr_amount
     except tortoise.exceptions.DoesNotExist:
